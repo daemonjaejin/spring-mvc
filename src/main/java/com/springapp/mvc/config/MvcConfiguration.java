a47a26e2acdbcf382b23a2e1d47f9e58c503a332
daemonjaejin <daemonjaejin@gmail.com>
2015-10-19 17:56:38 +0900

    설정 추가

--- src/main/java/com/springapp/mvc/config/MvcConfiguration.java
+++ src/main/java/com/springapp/mvc/config/MvcConfiguration.java
@@ -4,11 +4,17 @@ package com.springapp.mvc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 /**
  * Created by jin on 15. 10. 13..
@@ -31,6 +37,29 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
 
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver getMultipartResolver(){
+        return new CommonsMultipartResolver();
+    }
+
+    @Bean(name = "messageSource")
+    public ReloadableResourceBundleMessageSource getMessageSource(){
+        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
+        resource.setBasename("classpath:messages");
+        resource.setDefaultEncoding("UTF-8");
+        return resource;
+    }
+
+    @Bean(name = "localeResolver")
+    public SessionLocaleResolver getSessionResolver(){
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(new Locale("ko"));
+        return sessionLocaleResolver;
+    }
 
 }
